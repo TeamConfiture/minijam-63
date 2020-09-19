ccefded3828c1102fd876c961ea2b51869989a7a
Alexandre <14941050+doctortee@users.noreply.github.com>
2020-09-19 19:45:39 +0200

    Fixed most of the issues with the buttons, the 'right' slide isn't working. DO NOT MERGE

--- Tests/Labyrinthe.gd
+++ Tests/Labyrinthe.gd
@@ -5,6 +5,7 @@ export var inputs = []
 export var outputs = []
 
 var nodes = []
+var buttons = {}
 
 var labyTileTemplate =  preload("res://Tests/LabyrintheTile.tscn")
 var labyButtonTemplate =  preload("res://Tests/Bouton_laby.tscn")
@@ -12,29 +13,38 @@ var labyButtonTemplate =  preload("res://Tests/Bouton_laby.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	nodes.clear()
+	buttons.clear()
 	var temp = 0
 	var test = ["default", "i", "T", "I", "r"]
-	for i in range(size+1):
-		nodes.append([])
-		for j in range(size+1):
+	var theIndex = null
+	for i in range(size+2): # selon x
+		for j in range(size+2): # selon y
+			theIndex = [i,j] # on a donc x;y <=> colonne;ligne <=> i;j
 			if ((i == 0 or i == size+1) or (j == 0 or j == size+1) and not (i == 0 or i == size+1) and (j == 0 or j == size+1)):
-				nodes[i].append(labyButtonTemplate.instance())
-				nodes[i][j].line=i
-				nodes[i][j].col=j
-				nodes[i][j].monSens = (i == 0 or i == size-1)
-				nodes[i][j].maDirection = (i == size-1 or j == size-1)
-				nodes[i][j].set_scale(Vector2( .2, .2))
-				nodes[i][j].connect("triggered", self, "_on_Bouton_laby_triggered")
-			else:
-				nodes[i].append(labyTileTemplate.instance())
-				nodes[i][j].setTileType(test[temp])
+				print("Created ", theIndex)
+				buttons[theIndex] = labyButtonTemplate.instance()
+				buttons[theIndex].line=j-1
+				buttons[theIndex].col=i-1
+				buttons[theIndex].monSens = (i == 0 or i == size+1)
+				buttons[theIndex].maDirection = (i == size+1 or j == size+1)
+				buttons[theIndex].set_scale(Vector2( .2, .2))
+				buttons[theIndex].connect("triggered", self, "_on_Bouton_laby_triggered")
+				self.add_child(buttons[theIndex])
+	#			nodes[i][j].set_texture(load("res://Docs/PH_Labyrinthe_p.png"))
+				buttons[theIndex].set_position(Vector2((i-1)*100, (j-1)*100))
+	for i in range(size): # selon x
+		nodes.append([])
+		for j in range(size): # selon y
+			 # on a donc x;y <=> colonne;ligne <=> i;j
+			nodes[i].append(labyTileTemplate.instance())
+			nodes[i][j].setTileType(test[temp])
 #				print(i, ";", j, " got type ", temp, " : ", test[temp])
-				temp += 1
-				if temp >= 5:
-					temp = 0
+			temp += 1
+			if temp >= 5:
+				temp = 0
 			self.add_child(nodes[i][j])
 #			nodes[i][j].set_texture(load("res://Docs/PH_Labyrinthe_p.png"))
-			nodes[i][j].set_position(Vector2((i+1)*100, (j+1)*100))
+			nodes[i][j].set_position(Vector2(i*100, j*100))
 
 
 
@@ -47,41 +57,34 @@ func recalculatePath():
 
 func _on_Bouton_laby_triggered(maDirection, monSens, reset, line, column):
 	print("Ayudame por favor, I received a signal to move ", line, ";", column, " in...")
+	 # on a donc x;y <=> colonne;ligne <=> i;j
 	if monSens:
-		if maDirection:
-			print("droite")
-			var tempNode = nodes[line].pop_back()
-			var tempNode2 = nodes[line].pop_front()
-			nodes[line].push_front(nodes[line].pop_back())
-			nodes[line].push_front(tempNode2)
-			nodes[line].push_back(tempNode)
-		else:
-			print("gauche")
-			var tempNode = nodes[line].pop_back()
-			var tempNode2 = nodes[line].pop_front()
-			nodes[line].push_back(nodes[line].pop_front())
-			nodes[line].push_front(tempNode2)
-			nodes[line].push_back(tempNode)
-		
-		for i in range(size+1):
-			nodes[line][i].set_position(Vector2((line+1)*100, (i+1)*100))
-		
-	else:
 		var nodeTemp = null
 		if maDirection:
 			print("bas")
-			nodeTemp = nodes[size-1][column]
+			nodeTemp = nodes[size-1][line]
 			for i in range(size-1, 1, -1):
-				nodes[i][column] = nodes[i-1][column]
-			nodes[0][column] = nodeTemp
+				nodes[i][line] = nodes[i-1][line]
+			nodes[0][line] = nodeTemp
 		else:
 			print("haut")
-			nodeTemp = nodes[0][column]
+			nodeTemp = nodes[0][line]
 			for i in range(size-1):
-				nodes[i][column] = nodes[i+1][column]
-			nodes[size-1][column] = nodeTemp
+				nodes[i][line] = nodes[i+1][line]
+			nodes[size-1][line] = nodeTemp
 		
-		for i in range(size+1):
-			nodes[i][column].set_position(Vector2((i+1)*100, (column+1)*100))
+		for i in range(size):
+			nodes[i][line].set_position(Vector2(i*100, line*100))
+		
+	else:
+		if maDirection:
+			print("droite")
+			nodes[column].push_front(nodes[column].pop_back())
+		else:
+			print("gauche")
+			nodes[column].push_back(nodes[column].pop_front())
+		
+		for i in range(size):
+			nodes[column][i].set_position(Vector2(column*100, i*100))
 			
 	recalculatePath()
