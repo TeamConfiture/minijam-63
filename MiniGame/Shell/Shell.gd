8524e2f31dee667832644a6cb369dc65fae819b1
Rileran <a.rileran@gmail.com>
2020-09-19 22:02:33 +0200

    added login & password recognition

--- MiniGame/Shell/Shell.gd
+++ MiniGame/Shell/Shell.gd
@@ -1,25 +1,65 @@
 extends MarginContainer
 
+export(String) var login_querry = "Enter your login: "
+export(String) var password_querry = "Enter your password: "
+
+export(String) var login_manager = "mini-jam-63"
+export(String) var password_manager = "confitureftw"
+
+signal logged_in
+
 onready var line_edit = $VBoxContainer/Input/LineEdit
 onready var screen = $VBoxContainer/Output/TextEdit
 
 var default_line_edit
+var is_loging = true
+var login
+var password
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	default_line_edit = line_edit.text
-
+	screen.text += login_querry
 
 func _on_LineEdit_text_changed(new_text):
 	if new_text.length() < default_line_edit.length():
 		reset_line_edit()
 
 func _on_LineEdit_text_entered(new_text):
-	screen.text += new_text
-	screen.text += "\n"
-	reset_line_edit()
+	if is_loging:
+		login = new_text.substr(2).dedent().to_lower()
+		line_edit.secret = true
+		line_edit.text = ""
+		screen.text += login
+		screen.text += "\n"
+		screen.text += password_querry
+		is_loging = false
+	else:
+		password = new_text.dedent().to_lower()
+		screen.text += "********\n"
+		line_edit.secret = false
+		is_loging = true
+		check_user()
 	screen.scroll_vertical = screen.get_line_count()
+	reset_line_edit()
 
 func reset_line_edit():
-	line_edit.text = default_line_edit
-	line_edit.caret_position = default_line_edit.length()
+	if is_loging:
+		line_edit.text = default_line_edit
+		line_edit.caret_position = default_line_edit.length()
+	
+func check_user():
+	if login_manager == login and password_manager == password:
+		successful_login()
+	else:
+		reset_login()
+		
+func successful_login():
+	screen.text += "--------------------------------------\nCongratulation! This is a sample text.\n--------------------------------------"
+	emit_signal("logged_in")
+
+func reset_login():
+	screen.text += "***INVALID PASSWORD***"
+	screen.text += "\nYour system manager will be informed and you will be *fired*."
+	screen.text += "\nPlease try again\n\n"
+	screen.text += login_querry
