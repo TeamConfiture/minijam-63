6cc19b860cbcbd1f4f06724b53ef2bce44d7097c
Kimi <katran@etud.insa-toulouse.fr>
2020-09-19 14:54:06 +0200

    hero can interact with object

--- Tests/MainSceneScript.gd
+++ Tests/MainSceneScript.gd
@@ -7,3 +7,11 @@ func _ready():
 func _input(ev):
 	if Input.is_action_pressed("ui_pause") or Input.is_key_pressed(KEY_ESCAPE):
 		Global.goto_scene("res://Global/Menus/PauseScene.tscn")
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			print("Object Click")
+		else: 
+			print("Object Release")
