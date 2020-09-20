b9abaa5f9da87b6af4c2e115375fb2ae80f33a22
Rileran <a.rileran@gmail.com>
2020-09-20 22:53:38 +0200

    added skippable intro

--- Global/Intro.gd
+++ Global/Intro.gd
@@ -1,5 +1,9 @@
 extends VideoPlayer
 
+func _process(_delta):
+	if Input.is_action_pressed("ui_select"):
+		self.stop()
+		_on_Intro_finished()
 
 func _on_Intro_finished():
 	Global.goto_scene("res://Screens/Office/Office.tscn")
