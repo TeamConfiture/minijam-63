c2d3a70fdbeece69fb1992855eeb5b183e79b0b2
Rileran <a.rileran@gmail.com>
2020-09-20 22:08:04 +0200

    inital commit doesnt work

--- Global/SplashScreen.gd
+++ Global/SplashScreen.gd
@@ -2,3 +2,7 @@ extends ColorRect
 
 func _on_LogoTimer_timeout():
 	$AnimationPlayer.play("fade_in")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	Global.goto_scene("res://Global/Menus/MainMenu.tscn")
