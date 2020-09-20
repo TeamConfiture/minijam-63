057200b2712bc0b1c57475f960f8def23032b691
Rileran <a.rileran@gmail.com>
2020-09-21 01:18:49 +0200

    toggle fullscreen on startup

--- Global/SplashScreen.gd
+++ Global/SplashScreen.gd
@@ -1,8 +1,10 @@
 extends ColorRect
 
+func _ready():
+	OS.window_fullscreen = true
+
 func _on_LogoTimer_timeout():
 	$AnimationPlayer.play("fade_in")
-
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	Global.goto_scene("res://Global/Menus/MainMenu.tscn")
