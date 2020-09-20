abc766b06de6da7cc6644652d62c1e239ab545b6
Louis Farina <farina.louis42@gmail.com>
2020-09-20 13:39:32 +0200

    updated the minigame to make it a bit harder

--- MiniGame/Lockpicking/Lock.gd
+++ MiniGame/Lockpicking/Lock.gd
@@ -7,14 +7,18 @@ var screen_size
 var mouse_over = false
 var picking = false
 var picked = false
-var timer = 5
+var phase_timer = [3.75, 1.25, 2]
+var timer
+var phase_speed = [PI/2.5, -PI/2.5, PI/2.0]
+var phase = 0
 var angle = PI/2.0
 var center = Vector2()
 
 func reset():
 	picking = false
 	angle= PI/2.0
-	timer = 5
+	timer = phase_timer[0]
+	phase = 0
 	position.x = center.x + 150 * cos(angle)
 	position.y = center.y + 150 * sin(angle)
 
@@ -29,27 +33,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if picking:
-		angle += (PI/2.5)*delta
+		angle += phase_speed[phase]*delta
 		position.x = center.x + 150 * cos(angle)
 		position.y = center.y + 150 * sin(angle)
 		
 		timer -= delta
 		
 		if timer < 0:
-			picked = true
-			print_debug ("success!");
-			reset()
+			if phase == phase_timer.size() - 1:
+				picked = true
+				print_debug ("success!");
+				reset()
+			else:
+				phase += 1
+				timer = phase_timer[phase]
+			
 
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and mouse_over and not picking:
 		picking = true
-
-
 
 func _on_Area2D_mouse_entered():
 	mouse_over = true
 
 
 func _on_Area2D_mouse_exited():
-	mouse_over = false # Replace with function body.
+	mouse_over = false 
 	reset()
